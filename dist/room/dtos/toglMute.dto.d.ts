e6f60239c69b47938fc020738c75f105a65f0ec3
SDSirius <114844732+SDSirius@users.noreply.github.com>
2024-01-12 18:43:22 -0300

    removing dist from online

--- dist/room/dtos/toglMute.dto.d.ts
+++
@@ -1,4 +0,0 @@
-import { JoinRoomDto } from "./joinroom.dto";
-export declare class ToglMuteDto extends JoinRoomDto {
-    muted: boolean;
-}
--- dist/room/dtos/updateposition.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { JoinRoomDto } from "./joinroom.dto";
-export declare class UpdateUserPositionDto extends JoinRoomDto {
-    x: number;
-    y: number;
-    orientation: string;
-}
